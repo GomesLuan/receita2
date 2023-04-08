3bfb107932a6d86afcf5fc65ea05ca57d1c492fc
Luan Victor <luan.araujo.gom@gmail.com>
2023-04-08 10:45:04 -0300

    AppBar, Body e NavBar

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class NewAppBar extends StatelessWidget implements PreferredSizeWidget {
+  NewAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text("Dicas"),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class NewBody extends StatelessWidget {
+  NewBody();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Expanded(
+        child: Text("La Fin Du Monde - Bock - 65 ibu"),
+      ),
+      Expanded(
+        child: Text("Sapporo Premiume - Sour Ale - 54 ibu"),
+      ),
+      Expanded(
+        child: Text("Duvel - Pilsner - 82 ibu"),
+      )
+    ]);
+  } 
+}
+
+class NewNavBar extends StatelessWidget {
+  NewNavBar();
+
+  void botaoFoiTocado(int index) {
+    print("Tocaram no botão $index");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
+      BottomNavigationBarItem(
+        label: "Cafés",
+        icon: Icon(Icons.coffee_outlined),
+      ),
+      BottomNavigationBarItem(
+        label: "Cervejas", 
+        icon: Icon(Icons.local_drink_outlined)
+      ),
+      BottomNavigationBarItem(
+        label: "Nações", 
+        icon: Icon(Icons.flag_outlined)
+      )
+    ]);
+  }
+}
+
+void main() {
+  MaterialApp app = MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.deepPurple),
+    home: Scaffold(
+      appBar: NewAppBar(),
+      body: NewBody(),
+      bottomNavigationBar: NewNavBar(),
+    )
+  );
+  runApp(app);
+}
